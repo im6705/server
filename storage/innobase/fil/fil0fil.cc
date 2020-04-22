b55e4ad775bcf0ecc427be0fce83bff83ce46fa4
Thirunarayanan Balathandayuthapani <thiru@mariadb.com>
2020-04-22 21:06:04 +0530

    MDEV-8139	Fix Scrubbing
    btr_invalidate_index_id() - Invalidate the PAGE_INDEX_ID for the root
    page. Removed the invalidation redo log write from btr_free_root.
    
    fil_space_t:
    1) Introduced a variable freed_ranges to store freed page ranges.
    2) Introduced a variable called "last_freed_lsn" to store the last
    page freed lsn
    3) Introduced a variable called freed_mutex to protect the
    freed_ranges and last_freed_lsn.
    4) Added new functions like add_free_page(), remove_free_page(),
    merge_range(), split_range().
    
    buf_page_create() - Removes the page from freed_ranges when page
    is being reused.
    
    Renamed buf_flush_freed_page() to buf_release_freed_page(). It skips
    the zero writes/ punching the hole for freed pages.
    
    buf_flush_get_freed_pages() - Get the freed ranges from tablespace
    which is protected by freed_mutex and removes the tablespace
    freed ranges. Freed ranges has been read only when flushed_to_disk
    lsn is greater than last_freed_lsn of tablespace.
    
    buf_flush_freed_pages() - Write punch-hole or zeroes of the freed
    ranges after getting the range from buf_flush_get_freed_pages().
    
    buf_flush_try_neighbors() - Handles the flushing of freed ranges.
    
    mtr_t:
    - Introduced new variable called freed_pages to store the list of
    freed pages.
    add_freed_pages() - To add freed page in the list
    clear_freed_pages() - To clear the freed pages
    m_freed_in_system_tablespace - Page has been freed in system tablespace
    
    mtr_t::commit():
    - Add the freed page and update the last freed lsn in the tablespace
    
    file_name_t:
    - Introduces freed_pages to store freed pages during recovery
    - add_freed_page(), remove_freed_page()
    
    store_freed_or_init_rec():
      Store or remove the freed pages while encountering FREE_PAGE or
    INIT_PAGE redo log record.
    
    recv_init_crash_recovery_spaces():
    - Add the freed page encountered during recovery to respective tablespace

--- storage/innobase/fil/fil0fil.cc
+++ storage/innobase/fil/fil0fil.cc
@@ -1097,6 +1097,10 @@ fil_space_free_low(
 
 	ut_ad(space->size == 0);
 
+	if (space->freed_ranges)
+          delete space->freed_ranges;
+
+	space->freed_range_mutex().~mutex();
 	rw_lock_free(&space->latch);
 	fil_space_destroy_crypt_data(&space->crypt_data);
 
@@ -1241,6 +1245,10 @@ fil_space_create(
 		temporary tablespace files. */
 		space->atomic_write_supported = true;
 	}
+
+	space->freed_ranges= nullptr;
+
+	new (&space->frange_mutex_bytes) std::mutex;
 
 	HASH_INSERT(fil_space_t, hash, fil_system.spaces, id, space);
 
